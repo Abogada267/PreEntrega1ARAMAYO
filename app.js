880b6341f9f8efeddabe7313e1d01d1c4b2ba641
Abogada267 <131366010+Abogada267@users.noreply.github.com>
2023-05-04 22:21:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 ingreso();
 
 function ingreso() {
-  let main = document.getElementById('ingreso');
+  const edad = prompt("Ingresa tu edad:");
 
-  if (confirm('Hola, estas por ingresar a la pagina de la Dra. Aramayo sobre presupuestos, si desea ver los servicios que ofrece, haga click en "Aceptar", en caso contrario, en "Cancelar"')) {
-    alert("Clickeo Aceptar!, se mostrará la pagina.");
-    main.style.display = 'block';
+  if (edad >= 18) {
+    let main = document.getElementById('ingreso');
+    if (confirm('Hola, estas por ingresar a la pagina de la Dra. Aramayo sobre presupuestos, si desea ver los servicios que ofrece, haga click en "Aceptar", en caso contrario, en "Cancelar"')) {
+      alert("Clickeo Aceptar!, se mostrará la pagina.");
+      main.style.display = 'block';
+    } else {
+      alert("Hizo click en Cancelar!, que pena, usted se lo pierde!");
+      main.style.display = 'none';
+    }
+
+    const telefono = prompt("Ingresa tu número de teléfono,Cod. Area ():");
+    const regexTelefono = /^\(\d{4}\)\s*\d{6}$/;
+    if (regexTelefono.test(telefono)) {
+      console.log("El número de teléfono ingresado es válido.");
+      alert("El número de teléfono ingresado es válido.");
+    } else {
+      console.error("El número de teléfono ingresado no es válido.");
+      alert("El número de teléfono ingresado no es válido.");
+    }
+
+    function calcularPresupuesto() {
+      let ele1 = document.getElementById('TIPO DE CONSULTA')
+      let precio1 = ele1.options[ele1.selectedIndex].value;
+      let ele2 = document.getElementById('INTERVENCION')
+      let precio2 = ele2.options[ele2.selectedIndex].value;
+      let ele3 = document.getElementById('ANALISIS')
+      let precio3 = ele3.options[ele3.selectedIndex].value;
+      precio1 = parseInt(precio1);
+      precio2 = parseInt(precio2);
+      precio3 = parseInt(precio3);
+      let suma = precio1 + precio2 + precio3;
+      document.getElementById('resultado').value = suma;
+    }
   } else {
-    alert("Hizo click en Cancelar!, que pena, usted se lo pierde!");
-    main.style.display = 'none';
+    alert("DEBES SER MAYOR DE 18 AÑOS PARA ACCEDER A ESTA PÁGINA.");
+    window.location.replace("https://www.google.com");
   }
 }
-let nombre = prompt(`Ingresa tu nombre:`);
-alert(`Hola ${nombre}, Bienvenidos a la pagina.Es un gusto `);
-function calcularPresupuesto() {
-  let ele1 = document.getElementById('TIPO DE CONSULTA')
-  let precio1 = ele1.options[ele1.selectedIndex].value;
-  let ele2 = document.getElementById('INTERVENCION')
-  let precio2 = ele2.options[ele2.selectedIndex].value;
-  let ele3 = document.getElementById('ANALISIS')
-  let precio3 = ele3.options[ele3.selectedIndex].value;
-  precio1 = parseInt(precio1);
-  precio2 = parseInt(precio2);
-  precio3 = parseInt(precio3);
-  let suma = precio1 + precio2 + precio3;
-  document.getElementById('resultado').value = suma;
-}
+
